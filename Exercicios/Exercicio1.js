b92050dde278d791df02cc8a67061511ee27ec6f
AbnerPivatto <abnerpivatto@gmail.com>
2023-03-28 15:07:08 -0300

    Exercícios de JS

---
+++ Exercicios/Exercicio1.js
@@ -0,0 +1,17 @@
+        // Faça um programa que SOME 2 números
+
+        const number1 = 20
+        const number2 = 10
+
+              console.log (number1 + number2) 
+
+          //Resoluçaõ 
+          
+        const firstNumber = 20
+        const seccondNumber = 10
+        
+        const result = firstNumber + seccondNumber
+
+            console.log (result)
+
+    
---
+++ Exercicios/Exercicio3.js
@@ -0,0 +1,14 @@
+       //Faça um programa que encontre a raiz quadrada de um número, multiplique o resultado por 10 e divida por 33
+
+       let number1 = 10
+       let number2 = 33
+       
+           console.log(Math.sqrt(9,2) * number1 / number2)  
+
+        //Resolução
+
+       const firstNumber = Math.sqrt (9) 
+
+       const result = (firstNumber * 10 / 33)
+
+           console.log (result)
